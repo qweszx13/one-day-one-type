9115d8657a3f6bb753d7b634dfc89b1120cd234c
qweszx13 <qweszx33@naver.com>
2024-07-01 10:59:02 +0900

    feat:create exclude challenge

---
+++ src/exclude/exclude.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+  43 - Exclude
+  -------
+  by Zheeeng (@zheeeng) #쉬움 #built-in #union
+
+  ### 질문
+
+  `T`에서 `U`에 할당할 수 있는 타입을 제외하는 내장 제네릭 `Exclude<T, U>`를 이를 사용하지 않고 구현하세요.
+
+  예시:
+
+  ```ts
+  type Result = MyExclude<'a' | 'b' | 'c', 'a'> // 'b' | 'c'
+  ```
+
+  > GitHub에서 보기: https://tsch.js.org/43/ko
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+/* _____________ 다음 단계 _____________ */
+/*
+  > 정답 공유하기: https://tsch.js.org/43/answer/ko
+  > 정답 보기: https://tsch.js.org/43/solutions
+  > 다른 문제들: https://tsch.js.org/ko
+*/
