864b701a795f650fb93f1e07caa9abe78767d643
qweszx13 <qweszx33@naver.com>
2024-07-01 15:23:17 +0900

    concat

---
+++ src/concat/contcat.js
@@ -0,0 +1,26 @@
+"use strict";
+/*
+  533 - Concat
+  -------
+  by Andrey Krasovsky (@bre30kra69cs) #쉬움 #array
+
+  ### 질문
+
+  JavaScript의 `Array.concat` 함수를 타입 시스템에서 구현하세요. 타입은 두 인수를 받고, 인수를 왼쪽부터 concat한 새로운 배열을 반환해야 합니다.
+
+  예시:
+
+  ```ts
+  type Result = Concat<[1], [2]> // expected to be [1, 2]
+  ```
+
+  > GitHub에서 보기: https://tsch.js.org/533/ko
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+const tuple = [1];
+/* _____________ 다음 단계 _____________ */
+/*
+  > 정답 공유하기: https://tsch.js.org/533/answer/ko
+  > 정답 보기: https://tsch.js.org/533/solutions
+  > 다른 문제들: https://tsch.js.org/ko
+*/
